477d282bf100b75c6a0c01661c239b975277efa6
Lequral <monsieur.lol@yahoo.com>
2023-07-26 23:51:25 +0200

    my bad

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,7 @@ const config = {
     },
     adapter: adapter(),
     paths: {
-      base: "https://Lequral.github.io/Manage-Landing-Page",
+      base: dev ? "" : "/Manage-Landing-Page",
     },
   },
 };
