3b556a8b0b816887c0e887ef393eb1ffae1e3b39
Lequral <monsieur.lol@yahoo.com>
2023-07-26 23:43:57 +0200

    github pages setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
-// import("@sveltejs/kit").Config
+const dev = process.argv.includes("dev");
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
@@ -10,15 +12,10 @@ const config = {
     alias: {
       "@assets": "./src/assets",
     },
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: "build",
-      assets: "build",
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-    }),
+    adapter: adapter(),
+    paths: {
+      base: dev ? "" : "https://Lequral.github.io/Manage-Landing-Page",
+    },
   },
 };
 
